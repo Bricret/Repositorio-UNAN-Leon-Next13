24f1b94d2ee0dce3fe70ba0b168bdd44e9cdf342
Brian <brian.rico378@gmail.com>
2023-10-22 17:17:59 -0300

    Loading

--- src/app/components/options/Loading.jsx
+++ src/app/components/options/Loading.jsx
@@ -2,8 +2,20 @@
 // crear spinner de carga para cuando se esté cargando la página
 export const Loading = () => {
   return (
-        <div className="flex justify-center items-center h-full mx-auto">
-            <h1>me voy a morir</h1>
+    <div class="shadow rounded-md p-4 max-w-sm w-full mx-auto h-28 my-auto">
+    <div class="animate-pulse flex space-x-4">
+        <div class="rounded-full bg-slate-700 h-10 w-10"></div>
+        <div class="flex-1 space-y-6 py-1">
+        <div class="h-2 bg-slate-700 rounded"></div>
+        <div class="space-y-3">
+            <div class="grid grid-cols-3 gap-4">
+            <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+            <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+            </div>
+            <div class="h-2 bg-slate-700 rounded"></div>
         </div>
+        </div>
+    </div>
+    </div>
     )
 }
